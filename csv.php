1dd8b6c167a51a147944e3b01fd7c8254eb86477
ghanshyam-impinge <ghan.impinge@gmail.com>
2022-05-02 19:09:42 +0530

    Latest code with download csv sample file

--- csv.php
+++ csv.php
@@ -8,6 +8,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
 class csv {
     public function createCSV( $cols, $limit) {
         if(!empty ($cols) ) {
@@ -16,21 +17,22 @@ class csv {
             $csvfile    = fopen('test.csv', 'w');
             
             fputcsv($csvfile, $cols);
-             for($i=1; $i<$limit; $i++){
+            for($i=0; $i<$limit; $i++){
                 $row = array ($i, mt_rand(10,100), mt_rand(1,100));
+                
                 fputcsv($csvfile, $row);
             }
             return  "A file has been generated with input data";
         } else {
-            return "Invalid Argument value for column";
+            return "<center><h4>Invalid Argument value for column</h4></center>";
         }
 
     }
 }
 
 
-$colnames   = array('Heading1', 'Heading2', 'Heading3');
+$colnames   = array('Heading1', 'Heading2', 'Heading3','4','7');
 $data       = new csv ();
-$return     = $data->createCSV($colnames, 10);
+$return     = $data->createCSV($colnames, 5);
 echo $return;
 
