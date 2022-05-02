74f4d7bb8e319b563303d611be7853c39a046b3e
ghanshyam-impinge <ghan.impinge@gmail.com>
2022-05-02 19:11:36 +0530

    Latest code with download csv sample file

--- csv.php
+++ csv.php
@@ -1,38 +1,43 @@
 <?php
-/*******************
- * 
- * Sample template
- * for csv file
- * 
- * ****************/
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-class csv {
-    public function createCSV( $cols, $limit) {
+class customCsv {
+    function createCSV( $filename, $cols, $limit ) {
         if(!empty ($cols) ) {
-            
-            $countCols  = count($cols);
-            $csvfile    = fopen('test.csv', 'w');
-            
+
+            header('Content-Type: text/csv');
+            header('Content-Disposition: attachment; filename="'.$filename.'";'); // to download the csv
+
+            // clean output buffer
+            ob_end_clean();
+
+            $csvfile    = fopen('php://output', 'w'); 
+
             fputcsv($csvfile, $cols);
-            for($i=0; $i<$limit; $i++){
-                $row = array ($i, mt_rand(10,100), mt_rand(1,100));
-                
-                fputcsv($csvfile, $row);
-            }
-            return  "A file has been generated with input data";
+                $count = count($cols) - 1;
+                for($i=1; $i<=$limit; $i++) {
+                        $row = array($i);
+                        for($j=1; $j<=$count; $j++) {
+                                array_push($row,mt_rand(50,500));
+                        }
+                        fputcsv($csvfile, $row);
+                }
+
+                fclose( $csvfile );
+                // flush buffer
+                ob_flush();
+
+            exit();
         } else {
-            return "<center><h4>Invalid Argument value for column</h4></center>";
+            return "Invalid Argument value for column";
         }
 
     }
 }
 
+$filename = 'dummyCSV.csv';
+$colnames	= array('S.N', 'Col2', 'Col3');
 
-$colnames   = array('Heading1', 'Heading2', 'Heading3','4','7');
-$data       = new csv ();
-$return     = $data->createCSV($colnames, 5);
-echo $return;
+$data   =   new customCsv();
+$output = $data->createCSV($filename, $colnames, 20);
+echo $output;
 
+?>
